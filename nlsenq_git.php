8b8d48c053406a91c7c0f8973ced37402c303847
talismanred <danielsmart@gmx.com>
2016-07-22 08:24:57 +0100

    Add files via upload

---
+++ nlsenq_git.php
@@ -0,0 +1,40 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$data = json_decode(file_get_contents('php://input'), TRUE);
+$text = print_r($data,true);
+
+echo "<pre>";
+var_dump($text);
+echo "<br /><br />";
+print_r($data['name']['$modelValue']);
+echo "<br /><br />";
+print_r($data['email']['$modelValue']);
+echo "<br /><br />";
+print_r($data['message']['$modelValue']);
+echo "<br />..and finally...<br />";
+print_r($data['email_subject']);
+echo "</pre>";
+
+
+$nameDB = $data['name']['$modelValue'];
+$emailDB = $data['email']['$modelValue'];
+$messageDB = $data['message']['$modelValue'];
+$subjectDB = $data['email_subject'];
+
+$conn->query("INSERT INTO `cl16-nlsenq`.`enquiries` (`name`, `email`, `message`, `subject`) VALUES ('".$nameDB."', '".$emailDB."', '".$messageDB."', '".$subjectDB."');");
+
+$conn->close();
+
+?>
